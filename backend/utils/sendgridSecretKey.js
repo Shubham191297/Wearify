15969704cf38b08f4c21fcff4cddf21ec30e2cfd
shubh1917 <thapliyalshubham8@gmail.com>
2026-03-23 23:33:16 +0530

    Already committed in previous change

--- backend/utils/sendgridSecretKey.js
+++ backend/utils/sendgridSecretKey.js
@@ -2,5 +2,4 @@ const SendGrid_API_Key =
   "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";
 
 module.exports = SendGrid_API_Key;
-
 // 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
